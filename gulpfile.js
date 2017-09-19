a87e5b748de6c3b661331f04de0e7b5b98aa68a5
Jonathan Tsang <jw.tsang@gmail.com>
2017-09-19 15:52:50 -0700

    build($compile): Save the testing configuration before make

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,35 @@
 const gulp = require('gulp');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
 
-gulp.task('default', [])
+var jsFiles = 'build/scripts/*.js',
+    jsDest = 'build/assets';
+
+gulp.task('build:scripts', ['build:copy'], () =>
+  gulp.src(jsFiles)
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest(jsDest))
+    .pipe(rename('script.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(jsDest));
+);
+
+gulp.task('build:copy', ['build:clean'], () =>
+  gulp.src('src/**/*')
+    .pipe(copy('build', { prefix: 1 }));
+);
+
+gulp.task('build:remove-scripts-dir', ['build:copy'], () =>
+  gulp.src('build/scripts', { read: false })
+    .pipe(clean());
+);
+
+gulp.task('build:clean', () =>
+  gulp.src('build', { read: false })
+    .pipe(clean());
+);
+
+gulp.task('default', [
+  'build:scripts',
+]);
