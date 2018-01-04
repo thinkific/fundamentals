c9ab20f64901d81f229e08c909ec89beb3b907f4
Jonathan Tsang <jw.tsang@gmail.com>
2018-01-04 08:56:04 -0800

    fix($compile): Restore original gulpfile configuration and imports on theme.scss.liquid

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,7 @@ const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 
 var jsFiles = 'build/scripts/*.js',
-    jsDest = 'build/assets',
-    baseCSSFiles = 'build/styles/_base*.scss',
-    pageCSSFiles = 'build/styles/_page_*.scss',
-    sectionCSSFiles = 'build/styles/_section_*.scss',
-    cssDest = 'build/styles';
+    jsDest = 'build/assets';
 
 gulp.task('build:clean', () =>
   gulp.src('build', { read: false })
@@ -29,42 +25,7 @@ gulp.task('build:scripts', ['build:copy'], () =>
     .pipe(gulp.dest(jsDest))
 );
 
-gulp.task('build:stylesheets:base', ['build:copy'], () =>
-  gulp.src(baseCSSFiles)
-    .pipe(concat('base.scss'))
-    .pipe(gulp.dest(cssDest))
-);
-
-gulp.task('build:clean-base-stylesheets', ['build:stylesheets:base'], () =>
-  gulp.src('build/styles/_base*.scss', { read: false })
-    .pipe(clean())
-);
-
-gulp.task('build:stylesheets:pages', ['build:copy'], () =>
-  gulp.src(pageCSSFiles)
-    .pipe(concat('pages.scss'))
-    .pipe(gulp.dest(cssDest))
-);
-
-gulp.task('build:clean-page-stylesheets', ['build:stylesheets:pages'], () =>
-  gulp.src('build/styles/_page_*.scss', { read: false })
-    .pipe(clean())
-);
-
-gulp.task('build:stylesheets:sections', ['build:copy'], () =>
-  gulp.src(sectionCSSFiles)
-    .pipe(concat('sections.scss'))
-    .pipe(gulp.dest(cssDest))
-);
-
-gulp.task('build:clean-section-stylesheets', ['build:stylesheets:sections'], () =>
-  gulp.src('build/styles/_section_*.scss', { read: false })
-    .pipe(clean())
-);
-
-gulp.task('build:stylesheets', ['build:clean-base-stylesheets', 'build:clean-page-stylesheets', 'build:clean-section-stylesheets'])
-
-gulp.task('build:package', ['build:scripts', 'build:stylesheets'], () =>
+gulp.task('build:package', ['build:scripts'], () =>
   gulp.src('build/scripts', { read: false })
     .pipe(clean())
 );
